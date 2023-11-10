5c23f1747e253045834c7c10b668592d3d869505
Álvaro Somoza <somoza.alvaro@gmail.com>
2023-11-09 22:29:05 -0300

    intial commit webpage

--- src/iartisanxl/app/preferences.py
+++
@@ -1,9 +0,0 @@
-import attr
-
-
-@attr.s(eq=True)
-class PreferencesObject:
-    intermediate_images = attr.ib(type=bool)
-    use_tomes = attr.ib(type=bool)
-    sequential_offload = attr.ib(type=bool)
-    model_offload = attr.ib(type=bool)
