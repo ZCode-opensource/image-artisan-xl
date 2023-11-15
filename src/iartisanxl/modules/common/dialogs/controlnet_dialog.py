154d4d6235a5104425606f251b0bcb2e133343b6
Álvaro Somoza <somoza.alvaro@gmail.com>
2023-11-15 17:53:43 -0300

    drawing and image manipulation almost ready

--- src/iartisanxl/modules/common/dialogs/controlnet_dialog.py
+++ src/iartisanxl/modules/common/dialogs/controlnet_dialog.py
@@ -1,228 +1,9 @@
-from importlib.resources import files
-from PyQt6.QtWidgets import (
-    QVBoxLayout,
-    QHBoxLayout,
-    QLabel,
-    QWidget,
-    QGraphicsScene,
-    QGraphicsPixmapItem,
-    QGraphicsView,
-    QSizePolicy,
-    QApplication,
-    QSlider,
-)
-from PyQt6.QtCore import QSettings, Qt, QRectF, QPoint
-from PyQt6.QtGui import (
-    QPixmap,
-    QImageReader,
-    QPainter,
-    QPainterPath,
-    QCursor,
-    QPen,
-    QColor,
-)
-from PyQt6.QtSvg import QSvgRenderer
-from superqt import QLabeledDoubleRangeSlider
+from PyQt6.QtWidgets import QVBoxLayout, QHBoxLayout
+from PyQt6.QtCore import QSettings, Qt
+from superqt import QLabeledDoubleRangeSlider, QLabeledSlider, QLabeledDoubleSlider
 
 from iartisanxl.modules.common.dialogs.base_dialog import BaseDialog
-from iartisanxl.modules.common.drop_lightbox import DropLightBox
-
-
-class ImageEditor(QGraphicsView):
-    CIRCLE_CURSOR = files("iartisanxl.theme.cursors").joinpath("circle.svg")
-
-    def __init__(self, parent=None):
-        super(ImageEditor, self).__init__(parent)
-
-        self._zoom = 0
-        self._empty = True
-        self._scene = QGraphicsScene(self)
-        self._photo = QGraphicsPixmapItem()
-        self._scene.addItem(self._photo)
-        self.setScene(self._scene)
-
-        self.setRenderHint(QPainter.RenderHint.Antialiasing)
-        self.setRenderHint(QPainter.RenderHint.SmoothPixmapTransform)
-
-        self.setSizePolicy(QSizePolicy.Policy.Ignored, QSizePolicy.Policy.Ignored)
-        self.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
-        self.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
-
-        self.drawing = False
-        self.lastPoint = QPoint()
-        self.path = None
-        self.pathItem = None
-
-        self.drop_lightbox = DropLightBox(self)
-        self.drop_lightbox.setText("Drop file here")
-
-        self.slider = QSlider(Qt.Orientation.Horizontal)
-        self.slider.setMinimum(1)
-        self.slider.setMaximum(100)
-        self.slider.setValue(32)  # Default value
-        self.slider.valueChanged.connect(self.updateCursor)
-        layout = QVBoxLayout()
-        layout.addWidget(self.slider)
-        self.setLayout(layout)
-
-    def setPhoto(self, path):
-        reader = QImageReader(path)
-        if reader.canRead():
-            pixmap = QPixmap(path)
-            self._empty = False
-            self.setDragMode(QGraphicsView.DragMode.ScrollHandDrag)
-            self._photo.setPixmap(pixmap)
-        else:
-            self._empty = True
-            self.setDragMode(QGraphicsView.DragMode.NoDrag)
-            self._photo.setPixmap(QPixmap())
-        self.fitInView()
-
-    def hasPhoto(self):
-        return not self._empty
-
-    def fitInView(self):
-        rect = QRectF(self._photo.pixmap().rect())
-        if not rect.isNull():
-            self.setSceneRect(rect)
-            if self.hasPhoto():
-                unity = self.transform().mapRect(QRectF(0, 0, 1, 1))
-                self.scale(1 / unity.width(), 1 / unity.height())
-                viewrect = self.viewport().rect()
-                scenerect = self.transform().mapRect(rect)
-                factor = min(
-                    viewrect.width() / scenerect.width(),
-                    viewrect.height() / scenerect.height(),
-                )
-                self.scale(factor, factor)
-            self._zoom = 0
-
-    def wheelEvent(self, event):
-        if self.hasPhoto():
-            if event.modifiers() == Qt.KeyboardModifier.ControlModifier:
-                if event.angleDelta().y() > 0:
-                    factor = 1.25
-                    self._zoom += 1
-                else:
-                    factor = 0.8
-                    self._zoom -= 1
-                if self._zoom > 0:
-                    self.setTransformationAnchor(
-                        QGraphicsView.ViewportAnchor.AnchorUnderMouse
-                    )
-                    self.scale(factor, factor)
-                elif self._zoom == 0:
-                    self.fitInView()
-                else:
-                    self._zoom = 0
-            else:
-                super().wheelEvent(event)
-
-    def mousePressEvent(self, event):
-        if event.button() == Qt.MouseButton.LeftButton:
-            modifiers = QApplication.keyboardModifiers()
-            if modifiers == Qt.KeyboardModifier.ControlModifier:
-                self.setDragMode(QGraphicsView.DragMode.ScrollHandDrag)
-                super().mousePressEvent(event)
-            else:
-                self.drawing = True
-                self.lastPoint = self.mapToScene(event.pos())
-                self.path = QPainterPath()
-                self.path.moveTo(self.lastPoint)
-                self.pathItem = self._scene.addPath(self.path)
-                size = self.slider.value()
-                pixmap = QPixmap(size, size)
-                pixmap.fill(Qt.GlobalColor.transparent)
-                painter = QPainter(pixmap)
-                renderer = QSvgRenderer(str(self.CIRCLE_CURSOR))
-                renderer.render(painter)
-                painter.end()
-                circle_cursor = QCursor(pixmap)
-                self.setCursor(circle_cursor)
-
-    def mouseMoveEvent(self, event):
-        if (event.buttons() & Qt.MouseButton.LeftButton) and self.drawing:
-            currentPoint = self.mapToScene(event.pos())
-            self.path.lineTo(currentPoint)
-            pen = QPen(QColor("black"), self.slider.value())
-            self.pathItem.setPen(pen)
-            self.pathItem.setPath(self.path)
-            self.lastPoint = currentPoint
-        else:
-            super().mouseMoveEvent(event)
-
-    def mouseReleaseEvent(self, event):
-        if event.button() == Qt.MouseButton.LeftButton:
-            self.setDragMode(QGraphicsView.DragMode.NoDrag)
-            self.drawing = False
-            self.unsetCursor()  # Reset the cursor when you stop drawing
-        else:
-            super().mouseReleaseEvent(event)
-
-    def updateCursor(self):
-        if self.drawing:
-            size = self.slider.value()
-            pixmap = QPixmap(size, size)
-            pixmap.fill(Qt.GlobalColor.transparent)
-            painter = QPainter(pixmap)
-            renderer = QSvgRenderer(str(self.CIRCLE_CURSOR))
-            renderer.render(painter)
-            painter.end()
-            circle_cursor = QCursor(pixmap)
-            self.setCursor(circle_cursor)
-
-
-class ControlImageWidget(QWidget):
-    def __init__(
-        self,
-        text: str,
-        *args,
-        **kwargs,
-    ):
-        super().__init__(*args, **kwargs)
-
-        self.setObjectName("control_image_widget")
-        self.text = text
-        self.image_path = ""
-
-        self.setAcceptDrops(True)
-
-        self.init_ui()
-
-    def init_ui(self):
-        main_layout = QVBoxLayout()
-
-        source_text_label = QLabel(self.text)
-        main_layout.addWidget(source_text_label)
-        self.image_editor = ImageEditor()
-        main_layout.addWidget(self.image_editor)
-
-        main_layout.setStretch(0, 1)
-        main_layout.setStretch(1, 11)
-        self.setLayout(main_layout)
-
-    def dragEnterEvent(self, event):
-        if event.mimeData().hasUrls():
-            event.acceptProposedAction()
-            self.image_editor.drop_lightbox.show()
-        else:
-            event.ignore()
-
-    def dragLeaveEvent(self, event):
-        self.image_editor.drop_lightbox.hide()
-        event.accept()
-
-    def dropEvent(self, event):
-        self.image_editor.drop_lightbox.hide()
-
-        for url in event.mimeData().urls():
-            path = url.toLocalFile()
-
-            reader = QImageReader(path)
-
-            if reader.canRead():
-                self.image_path = path
-                self.image_editor.setPhoto(self.image_path)
+from iartisanxl.modules.common.dialogs.control_image_widget import ControlImageWidget
 
 
 class ControlNetDialog(BaseDialog):
@@ -253,15 +34,31 @@ class ControlNetDialog(BaseDialog):
         slider.setRange(0, 1)
         slider.setValue((self.control_guidance_start, self.control_guidance_end))
         slider.valueChanged.connect(self.on_guidance_changed)
-
         top_layout.addWidget(slider)
+
+        brush_size_slider = QLabeledSlider()
+        brush_size_slider.setRange(1, 100)
+        brush_size_slider.setValue(20)
+        top_layout.addWidget(brush_size_slider)
+
+        brush_hardness_slider = QLabeledDoubleSlider()
+        brush_hardness_slider.setRange(0.0, 0.99)
+        brush_hardness_slider.setValue(0.5)
+        top_layout.addWidget(brush_hardness_slider)
+
         content_layout.addLayout(top_layout)
 
         images_layout = QHBoxLayout()
-        source_widget = ControlImageWidget("Source image")
+        images_layout.setContentsMargins(2, 0, 4, 0)
+        images_layout.setSpacing(2)
+        source_widget = ControlImageWidget(
+            "Source image", self.image_viewer, self.image_generation_data
+        )
         images_layout.addWidget(source_widget)
 
-        annotator_widget = ControlImageWidget("Annotator")
+        annotator_widget = ControlImageWidget(
+            "Annotator", self.image_viewer, self.image_generation_data
+        )
         images_layout.addWidget(annotator_widget)
 
         content_layout.addLayout(images_layout)
@@ -270,6 +67,19 @@ class ControlNetDialog(BaseDialog):
         content_layout.setStretch(1, 6)
 
         self.main_layout.addLayout(content_layout)
+
+        brush_size_slider.valueChanged.connect(
+            source_widget.image_editor.set_brush_size
+        )
+        brush_hardness_slider.valueChanged.connect(
+            source_widget.image_editor.set_brush_hardness
+        )
+        brush_size_slider.valueChanged.connect(
+            annotator_widget.image_editor.set_brush_size
+        )
+        brush_hardness_slider.valueChanged.connect(
+            annotator_widget.image_editor.set_brush_hardness
+        )
 
     def closeEvent(self, event):
         self.settings.beginGroup("controlnet_dialog")
---
+++ src/iartisanxl/modules/common/image_editor.py
@@ -0,0 +1,234 @@
+from PyQt6.QtWidgets import (
+    QGraphicsScene,
+    QGraphicsPixmapItem,
+    QGraphicsView,
+    QSizePolicy,
+    QApplication,
+    QGraphicsPathItem,
+)
+from PyQt6.QtCore import Qt, QRectF, QPoint
+from PyQt6.QtGui import (
+    QPixmap,
+    QPainter,
+    QPainterPath,
+    QColor,
+    QRadialGradient,
+    QBrush,
+    QPen,
+    QImage,
+)
+
+from iartisanxl.modules.common.drop_lightbox import DropLightBox
+
+
+class ImageEditor(QGraphicsView):
+    def __init__(self, parent=None):
+        super(ImageEditor, self).__init__(parent)
+
+        self._zoom = 0
+        self._empty = True
+        self._scene = QGraphicsScene(self)
+        self._photo = QGraphicsPixmapItem()
+        self._scene.addItem(self._photo)
+        self.setScene(self._scene)
+
+        self.setRenderHint(QPainter.RenderHint.Antialiasing)
+        self.setRenderHint(QPainter.RenderHint.SmoothPixmapTransform)
+
+        self.setSizePolicy(QSizePolicy.Policy.Ignored, QSizePolicy.Policy.Ignored)
+        self.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+
+        self.original_pixmap = None
+
+        self.drawing = False
+        self.last_point = QPoint()
+        self.brush_size = 32
+        self.hardness = 0
+        self.undo_stack = []
+        self.redo_stack = []
+        self.current_drawing = []
+
+        self.drop_lightbox = DropLightBox(self)
+        self.drop_lightbox.setText("Drop file here")
+
+    def set_pixmap(self, pixmap: QPixmap):
+        self.original_pixmap = pixmap
+        self._empty = False
+        self.setDragMode(QGraphicsView.DragMode.NoDrag)
+        self._photo.setPixmap(pixmap)
+        self.setCursor(Qt.CursorShape.ArrowCursor)
+        self.fitInView()
+
+    def set_white_pixmap(self, width, height):
+        # Create a white QPixmap
+        white_pixmap = QPixmap(width, height)
+        white_pixmap.fill(Qt.GlobalColor.white)
+
+        # Set the QPixmap as the photo
+        self.original_pixmap = white_pixmap
+        self._empty = False
+        self.setDragMode(QGraphicsView.DragMode.NoDrag)
+        self._photo.setPixmap(self.original_pixmap)
+        self.setCursor(Qt.CursorShape.ArrowCursor)
+        self.fitInView()
+
+    def has_photo(self):
+        return not self._empty
+
+    def fitInView(self):
+        rect = QRectF(self._photo.pixmap().rect())
+        if not rect.isNull():
+            self.setSceneRect(rect)
+            if self.has_photo():
+                unity = self.transform().mapRect(QRectF(0, 0, 1, 1))
+                self.scale(1 / unity.width(), 1 / unity.height())
+                viewrect = self.viewport().rect()
+                scenerect = self.transform().mapRect(rect)
+                factor = min(
+                    viewrect.width() / scenerect.width(),
+                    viewrect.height() / scenerect.height(),
+                )
+                self.scale(factor, factor)
+            self._zoom = 0
+
+    def wheelEvent(self, event):
+        if self.has_photo():
+            if event.modifiers() == Qt.KeyboardModifier.ControlModifier:
+                if event.angleDelta().y() > 0:
+                    factor = 1.25
+                    self._zoom += 1
+                else:
+                    factor = 0.8
+                    self._zoom -= 1
+                if self._zoom > 0:
+                    self.setTransformationAnchor(
+                        QGraphicsView.ViewportAnchor.AnchorUnderMouse
+                    )
+                    self.scale(factor, factor)
+                elif self._zoom == 0:
+                    self.fitInView()
+                else:
+                    self._zoom = 0
+            else:
+                super().wheelEvent(event)
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.MouseButton.LeftButton:
+            modifiers = QApplication.keyboardModifiers()
+            if modifiers == Qt.KeyboardModifier.ControlModifier:
+                self.setDragMode(QGraphicsView.DragMode.ScrollHandDrag)
+                super().mousePressEvent(event)
+            else:
+                self.drawing = True
+                self.last_point = self.mapToScene(event.pos())
+                path = QPainterPath()
+                path.moveTo(self.last_point)
+                path_item = self._scene.addPath(path)
+                self.current_drawing.append(path_item)
+                self.setCursor(Qt.CursorShape.BlankCursor)
+
+    def mouseMoveEvent(self, event):
+        if (event.buttons() & Qt.MouseButton.LeftButton) and self.drawing:
+            current_point = self.mapToScene(event.pos())
+
+            # Create a radial gradient as the brush
+            gradient = QRadialGradient(self.last_point, self.brush_size / 2)
+            gradient.setColorAt(0, QColor(0, 0, 0, 255))  # Inner color (black)
+            gradient.setColorAt(
+                self.hardness, QColor(0, 0, 0, 255)
+            )  # Outer color (black)
+            gradient.setColorAt(1, QColor(0, 0, 0, 0))  # Beyond hardness (transparent)
+            brush = QBrush(gradient)
+
+            # Create a path item with the brush
+            path_item = QGraphicsPathItem()
+            path_item.setBrush(brush)
+            path_item.setPen(QPen(Qt.GlobalColor.transparent))
+            path = QPainterPath()
+            path.addEllipse(self.last_point, self.brush_size / 2, self.brush_size / 2)
+            path_item.setPath(path)
+            self._scene.addItem(path_item)
+            self.current_drawing.append(path_item)
+
+            self.last_point = current_point
+        else:
+            super().mouseMoveEvent(event)
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == Qt.MouseButton.LeftButton:
+            self.setDragMode(QGraphicsView.DragMode.NoDrag)
+            self.drawing = False
+            self.setCursor(Qt.CursorShape.ArrowCursor)
+            self.undo_stack.append(self.current_drawing)
+            self.current_drawing = []
+        else:
+            super().mouseReleaseEvent(event)
+
+    def undo(self):
+        if self.undo_stack:
+            drawing = self.undo_stack.pop()
+            redo_drawing = []
+            for path_item in drawing:
+                self._scene.removeItem(path_item)
+                redo_drawing.append(path_item)
+
+            self.redo_stack.append(redo_drawing)
+
+    def redo(self):
+        if self.redo_stack:
+            drawing = self.redo_stack.pop()
+            undo_drawing = []
+            for path_item in drawing:
+                self._scene.addItem(path_item)
+                undo_drawing.append(path_item)
+            self.undo_stack.append(undo_drawing)
+
+    def keyReleaseEvent(self, event):
+        key = event.key()
+        modifiers = event.modifiers()
+        if key == Qt.Key.Key_Z and modifiers == Qt.KeyboardModifier.ControlModifier:
+            self.undo()
+        elif key == Qt.Key.Key_Z and modifiers == (
+            Qt.KeyboardModifier.ControlModifier | Qt.KeyboardModifier.ShiftModifier
+        ):
+            self.redo()
+        elif key == Qt.Key.Key_S and modifiers == Qt.KeyboardModifier.ControlModifier:
+            self.export_pixmap()
+        else:
+            super().keyReleaseEvent(event)
+
+    def clear_and_restore(self):
+        self._scene.clear()
+        self.undo_stack.clear()
+        self.redo_stack.clear()
+        original_image_item = QGraphicsPixmapItem(self.original_pixmap)
+        self._photo = original_image_item
+        self._scene.addItem(original_image_item)
+
+    def export_pixmap(self):
+        image = QImage(
+            self._scene.sceneRect().size().toSize(), QImage.Format.Format_ARGB32
+        )
+        image.fill(Qt.GlobalColor.transparent)
+
+        painter = QPainter(image)
+        self._scene.render(painter)
+        painter.end()
+
+        image.save("some path")
+
+    def set_brush_size(self, value):
+        self.brush_size = value
+
+    def set_brush_hardness(self, value):
+        self.hardness = value
+
+    def dragMoveEvent(self, event):
+        pass
+
+    def dragEnterEvent(self, event):
+        pass
+
+    def dropEvent(self, event):
+        pass
