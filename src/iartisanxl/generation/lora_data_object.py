1f413cfdb7ca922b239be93fc70d8cbe6f800a75
Álvaro Somoza <somoza.alvaro@gmail.com>
2023-11-20 08:10:38 -0300

    changed enable to default True for legacy compat

--- src/iartisanxl/generation/lora_data_object.py
+++ src/iartisanxl/generation/lora_data_object.py
@@ -3,11 +3,11 @@ import attr
 
 @attr.s
 class LoraDataObject:
-    enabled = attr.ib(type=bool)
     name = attr.ib(type=str)
     filename = attr.ib(type=str)
     version = attr.ib(type=str)
     path = attr.ib(type=str)
+    enabled = attr.ib(type=bool, default=True)
     weight = attr.ib(type=float, default=1.0)
 
     def copy(self):
