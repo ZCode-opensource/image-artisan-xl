692f62786954dd73fd166e65091babe64bcfcfa8
Álvaro Somoza <somoza.alvaro@gmail.com>
2023-11-23 13:22:16 -0300

    fixed callback from json

--- src/iartisanxl/graph/nodes/image_send_node.py
+++ src/iartisanxl/graph/nodes/image_send_node.py
@@ -5,10 +5,25 @@ class ImageSendNode(Node):
     REQUIRED_INPUTS = ["image"]
     OUTPUTS = []
 
-    def __init__(self, image_callback: callable, **kwargs):
+    def __init__(self, image_callback: callable = None, **kwargs):
         super().__init__(**kwargs)
 
         self.image_callback = image_callback
+
+    def to_dict(self):
+        node_dict = super().to_dict()
+        node_dict["image_callback"] = (
+            self.image_callback.__name__ if self.image_callback else None
+        )
+        return node_dict
+
+    @classmethod
+    def from_dict(cls, node_dict, callbacks=None):
+        node = super(ImageSendNode, cls).from_dict(node_dict)
+        node.image_callback = (
+            callbacks.get(node_dict["image_callback"]) if callbacks else None
+        )
+        return node
 
     def __call__(self):
         super().__call__()
