88edab8c1a16e07d1ca370e8a92024b9aa17c477
Álvaro Somoza <somoza.alvaro@gmail.com>
2023-11-24 18:34:50 -0300

    image generation data is now a singleton

--- src/iartisanxl/generation/model_data_object.py
+++ src/iartisanxl/generation/model_data_object.py
@@ -1,18 +1,9 @@
 import attr
 
 
-@attr.s
+@attr.s(slots=True)
 class ModelDataObject:
     name: str = attr.ib(default="")
     path: str = attr.ib(default="")
     version: str = attr.ib(default="")
     type: str = attr.ib(default="")
-
-    def copy(self):
-        new_obj = ModelDataObject(
-            name=self.name,
-            path=self.path,
-            version=self.version,
-            type=self.type,
-        )
-        return new_obj
--- src/iartisanxl/generation/vae_data_object.py
+++ src/iartisanxl/generation/vae_data_object.py
@@ -1,14 +1,7 @@
 import attr
 
 
-@attr.s
+@attr.s(slots=True)
 class VaeDataObject:
     name: str = attr.ib(default="")
     path: str = attr.ib(default="")
-
-    def copy(self):
-        new_obj = VaeDataObject(
-            name=self.name,
-            path=self.path,
-        )
-        return new_obj
