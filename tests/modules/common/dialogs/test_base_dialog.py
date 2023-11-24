fb6a56a290a39e32fbc8c37b45482f09b07aaf95
Álvaro Somoza <somoza.alvaro@gmail.com>
2023-11-24 18:37:42 -0300

    basic tests for dialogs

---
+++ tests/modules/common/dialogs/test_base_dialog.py
@@ -0,0 +1,31 @@
+import sys
+import unittest
+from PyQt6.QtWidgets import QApplication
+
+from iartisanxl.modules.common.dialogs.base_dialog import BaseDialog
+
+app = QApplication(sys.argv)
+
+
+class TestBaseDialog(unittest.TestCase):
+    def setUp(self):
+        directories = None
+        title = "Test"
+        show_error = lambda x: None
+        image_generation_data = None
+        image_viewer = None
+        prompt_window = None
+        auto_generate_function = lambda: None
+
+        self.dialog = BaseDialog(
+            directories,
+            title,
+            show_error,
+            image_generation_data,
+            image_viewer,
+            prompt_window,
+            auto_generate_function,
+        )
+
+    def test_init(self):
+        self.assertEqual(self.dialog.windowTitle(), "Test")
---
+++ tests/modules/common/dialogs/test_lora_dialog.py
@@ -0,0 +1,70 @@
+import sys
+
+import unittest
+from unittest.mock import patch
+
+from PyQt6.QtWidgets import QApplication
+
+from iartisanxl.app.directories import DirectoriesObject
+from iartisanxl.modules.common.dialogs.lora_dialog import LoraDialog
+
+
+app = QApplication(sys.argv)
+
+
+class TestLoraDialog(unittest.TestCase):
+    def setUp(self):
+        directories = DirectoriesObject(
+            models_diffusers="",
+            models_safetensors="",
+            vaes="",
+            models_loras="",
+            models_controlnets="",
+            outputs_images="",
+        )
+
+        title = "Test"
+        show_error = lambda x: None
+        image_generation_data = None
+        image_viewer = None
+        prompt_window = None
+        auto_generate_function = lambda: None
+
+        self.dialog = LoraDialog(
+            directories,
+            title,
+            show_error,
+            image_generation_data,
+            image_viewer,
+            prompt_window,
+            auto_generate_function,
+        )
+
+    def test_init(self):
+        self.assertEqual(self.dialog.windowTitle(), "LoRAs")
+        self.assertEqual(self.dialog.minimumSize().width(), 1160)
+        self.assertEqual(self.dialog.minimumSize().height(), 800)
+
+    def test_on_lora_item_clicked(self):
+        data = {
+            "name": "test",
+            "root_filename": "test_filename",
+            "version": "1.0",
+            "filepath": "/path/to/file",
+        }
+
+        with patch(
+            "iartisanxl.modules.common.dialogs.lora_info_widget.get_metadata_from_safetensors",
+            return_value={},
+        ):
+            self.dialog.on_lora_item_clicked(data)
+
+            self.assertIsNotNone(self.dialog.selected_lora)
+            self.assertEqual(self.dialog.selected_lora.name, data["name"])
+            self.assertEqual(self.dialog.selected_lora.filename, data["root_filename"])
+            self.assertEqual(self.dialog.selected_lora.version, data["version"])
+            self.assertEqual(self.dialog.selected_lora.path, data["filepath"])
+
+
+# def test_lora_selected(self):
+#     self.assertNotEqual(self.dialog.selected_lora, None)
