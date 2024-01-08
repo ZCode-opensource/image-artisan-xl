6049a356af4859264edb10382a846ddce12d3887
Álvaro Somoza <somoza.alvaro@gmail.com>
2024-01-08 05:58:37 -0300

    updated tests

--- tests/modules/common/dialogs/test_base_dialog.py
+++ tests/modules/common/dialogs/test_base_dialog.py
@@ -10,21 +10,21 @@ app = QApplication(sys.argv)
 class TestBaseDialog(unittest.TestCase):
     def setUp(self):
         directories = None
+        preferences = None
         title = "Test"
         show_error = lambda x: None
         image_generation_data = None
         image_viewer = None
         prompt_window = None
-        auto_generate_function = lambda: None
 
         self.dialog = BaseDialog(
             directories,
+            preferences,
             title,
             show_error,
             image_generation_data,
             image_viewer,
             prompt_window,
-            auto_generate_function,
         )
 
     def test_init(self):
--- tests/modules/common/dialogs/test_lora_dialog.py
+++ tests/modules/common/dialogs/test_lora_dialog.py
@@ -6,6 +6,7 @@ from unittest.mock import patch
 from PyQt6.QtWidgets import QApplication
 
 from iartisanxl.app.directories import DirectoriesObject
+from iartisanxl.app.preferences import PreferencesObject
 from iartisanxl.modules.common.dialogs.lora_dialog import LoraDialog
 
 
@@ -17,10 +18,26 @@ class TestLoraDialog(unittest.TestCase):
         directories = DirectoriesObject(
             models_diffusers="",
             models_safetensors="",
-            vaes="",
+            models_vaes="",
             models_loras="",
             models_controlnets="",
+            models_t2i_adapters="",
+            models_ip_adapters="",
+            models_upscalers="",
             outputs_images="",
+            outputs_loras="",
+            datasets="",
+        )
+
+        preferences = PreferencesObject(
+            intermediate_images=False,
+            use_tomes=False,
+            sequential_offload=False,
+            model_offload=False,
+            save_image_metadata=False,
+            save_image_control_annotators=False,
+            save_image_control_sources=False,
+            hide_nsfw=False,
         )
 
         title = "Test"
@@ -28,17 +45,8 @@ class TestLoraDialog(unittest.TestCase):
         image_generation_data = None
         image_viewer = None
         prompt_window = None
-        auto_generate_function = lambda: None
 
-        self.dialog = LoraDialog(
-            directories,
-            title,
-            show_error,
-            image_generation_data,
-            image_viewer,
-            prompt_window,
-            auto_generate_function,
-        )
+        self.dialog = LoraDialog(directories, preferences, title, show_error, image_generation_data, image_viewer, prompt_window)
 
     def test_init(self):
         self.assertEqual(self.dialog.windowTitle(), "LoRAs")
@@ -46,12 +54,7 @@ class TestLoraDialog(unittest.TestCase):
         self.assertEqual(self.dialog.minimumSize().height(), 800)
 
     def test_on_lora_item_clicked(self):
-        data = {
-            "name": "test",
-            "root_filename": "test_filename",
-            "version": "1.0",
-            "filepath": "/path/to/file",
-        }
+        data = {"name": "test", "root_filename": "test_filename", "version": "1.0", "filepath": "/path/to/file"}
 
         with patch(
             "iartisanxl.modules.common.dialogs.lora_info_widget.get_metadata_from_safetensors",
