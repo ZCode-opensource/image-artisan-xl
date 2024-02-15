f97dbd154d52ab4fa47a605678113c36647093f1
Álvaro Somoza <somoza.alvaro@gmail.com>
2024-02-15 01:11:13 -0300

    fixed depth preprocessor

--- src/iartisanxl/preprocessors/depth/depth_estimator.py
+++ src/iartisanxl/preprocessors/depth/depth_estimator.py
@@ -1,8 +1,9 @@
 import os
 
-import cv2
+import numpy as np
 import torch
-from transformers import DPTImageProcessor, DPTForDepthEstimation
+from PIL import Image
+from transformers import DPTForDepthEstimation, DPTImageProcessor
 
 
 class DepthEstimator:
@@ -20,27 +21,24 @@ class DepthEstimator:
             self.model_name = new_model_name
             self.load_model()
 
-    # pylint: disable=no-member
-    def get_depth_map(self, image, resolution=None):
-        original_resolution = (image.shape[0], image.shape[1])
+    def get_depth_map(self, image: Image.Image, resolution: float = None):
+        original_size = image.size
+
         if resolution:
-            image = cv2.resize(image, (resolution[1], resolution[0]))
-        image = self.image_processor(images=image, return_tensors="pt").pixel_values.to("cuda")
-        with torch.no_grad(), torch.autocast("cuda"):
-            depth_map = self.depth_estimator(image).predicted_depth
+            image = image.resize(resolution, Image.Resampling.LANCZOS)
 
-        depth_map = torch.nn.functional.interpolate(
-            depth_map.unsqueeze(1),
-            size=original_resolution,
-            mode="bicubic",
-            align_corners=False,
+        inputs = self.image_processor(images=image, return_tensors="pt").to("cuda")
+        with torch.no_grad():
+            outputs = self.depth_estimator(**inputs)
+            predicted_depth = outputs.predicted_depth
+
+        # get back the image to the original size
+        prediction = torch.nn.functional.interpolate(
+            predicted_depth.unsqueeze(1), size=original_size[::-1], mode="bicubic", align_corners=False
         )
-        depth_min = torch.amin(depth_map, dim=[1, 2, 3], keepdim=True)
-        depth_max = torch.amax(depth_map, dim=[1, 2, 3], keepdim=True)
-        depth_map = (depth_map - depth_min) / (depth_max - depth_min)
-        depth_map = (depth_map * 255.0).clip(0, 255).to(torch.uint8)
-        image = torch.cat([depth_map] * 3, dim=1)
 
-        image = image.permute(0, 2, 3, 1).cpu().numpy()[0]
+        output = prediction.squeeze().cpu().numpy()
+        formatted = (output * 255 / np.max(output)).astype("uint8")
+        depthmap = Image.fromarray(formatted)
 
-        return image
+        return depthmap
